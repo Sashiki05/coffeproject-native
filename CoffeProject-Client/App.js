fa95f1e9786713f94d766440f3ff4b48f5958d43
Sashiki05 <save.cutolo@gmail.comm>
2023-07-24 16:25:34 +0200

    front first

---
+++ CoffeProject-Client/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import AppNavigation from './src/navigation/AppNavigation';
+
+
+export default function App() {
+  return (
+    <AppNavigation />
+  );
+}
+
